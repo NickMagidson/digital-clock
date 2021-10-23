62f4f9569ae313538a5cf2fe53cf27f490ce205d
Nick <nickmagi24@gmail.com>
2021-10-23 16:46:50 -0400

    More psudeo

--- app.js
+++ app.js
@@ -4,7 +4,22 @@
     //hour variable
     //minute variable
     //second variable
+    //am_pm = "PM" (variable)
 //}
 
 
 // Set AM/PM depending on the hour value.
+// if hour > 12
+//  PM
+
+// if hour === 0
+// AM
+
+
+
+
+//let currentTime = hour + ":" + min + ":" + sec + am_pm
+
+
+
+//Wrap whole code in setInterval(, )??
