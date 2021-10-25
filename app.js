ffa846a6a5b94914e66b0160ecaf5036dddcc11b
Nick <nickmagi24@gmail.com>
2021-10-25 15:55:00 -0400

    Dropdown functionality and styling

--- app.js
+++ app.js
@@ -30,4 +30,22 @@ function showTime() {
     setTimeout(showTime, 1000);
 }
 
+function dropDown() {
+    document.getElementById("skin-dropdown").classList.toggle("show");
+  }
+  
+
+window.onclick = function(event) {
+    if (!event.target.matches('.buttons')) {
+      let dropdowns = document.getElementsByClassName(".dropdown-content");
+      let i;
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
 showTime();
